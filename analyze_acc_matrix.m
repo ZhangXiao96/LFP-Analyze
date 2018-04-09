6f544b10dcc9395aed782cebad112ec9c0c72ee9
Άγνωστο.Z <540746256@qq.com>
2018-04-09 13:59:45 +0800

    Update analyze_acc_matrix.m

--- analyze_acc_matrix.m
+++ analyze_acc_matrix.m
@@ -4,7 +4,7 @@ input_file_dirs = 'data/merge/';
 tag = 'targ'; % 'result' or 'targ'
 y_name = 'Direction';% 'Reward' or 'Option' or 'Direction'
 val_split = 0.5; % The proportion of the training set 
-label_balance = false; % boolean variable, e.g. true: use weights in SVM.
+label_balance = true; % boolean variable, e.g. true: use weights in SVM.
 
 % if size(X_tf)==[trial 100 100 3] and f==0~250 Hz and t==-500~523 ms
 % then unit_f = ((250+1)/100) Hz/pixel, unit_t = (1024/100) ms/pixel
